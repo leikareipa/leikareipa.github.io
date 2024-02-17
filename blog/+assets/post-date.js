4ebec0ec4bd5f912183fb1915ee7665223145db7
leikareipa <drilledicecream@gmail.com>
2024-02-17 20:32:08 +0200

    Update files

--- blog/+assets/post-date.js
+++ blog/+assets/post-date.js
@@ -6,7 +6,7 @@ const postDate = {
     },
     template: `
     <p class="post-date">
-        <span class="bit">By <a href="https://github.com/leikareipa">leikareipa</a></span>
+        <span class="bit">By Petri Esimerkki</span>
         <span class="bit">{{date}}<span v-if="edited"><span style="margin: 0 0.4em;">&centerdot;</span>edited on {{edited}}</span>
         </span>
     </p>
