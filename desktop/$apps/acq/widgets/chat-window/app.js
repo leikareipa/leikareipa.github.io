f077552c23b2d895df932581401eb8dfc5d016bb
leikareipa <drilledicecream@gmail.com>
2025-04-16 23:37:07 +0300

    Update files

--- desktop/$apps/acq/widgets/chat-window/app.js
+++ desktop/$apps/acq/widgets/chat-window/app.js
@@ -437,6 +437,7 @@ function curtime() {
     const formatter = new Intl.DateTimeFormat('en-US', {
         hour: '2-digit',
         minute: '2-digit',
+        second: '2-digit',
         hour12: true
     });
 
